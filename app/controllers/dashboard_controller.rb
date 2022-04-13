acabf9a8dba0761b03a3b3ba59228fe4d9da9a2e
Jin DU <telepathymira@gmail.com>
2022-04-13 10:14:26 +0200

    unfinished

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def dashboard
     @reservations = Reservation.where(user_id:"#{current_user.id}")
     @plants = Plant.where(user_id:"#{current_user.id}")
+    authorize @dashboard
   end
 end
