2fbcc08a70a8ee696265dfc63a25774e62b865cb
Jennifer Carrow <jcarrow02@gmail.com>
2022-04-13 15:33:13 +0200

    Cloudinary photos now attached to index

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,8 @@
 class Plant < ApplicationRecord
   belongs_to :user
 
+  has_one_attached :photo
+
   has_many :reservations, dependent: :destroy
 
   validates :name, :environment, :size, presence: true
