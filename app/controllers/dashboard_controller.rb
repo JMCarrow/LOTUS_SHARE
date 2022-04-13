1780d87602ddb5425bc3bd1343f9a91926a84323
RaoniPirat <raoni.pirat@gmail.com>
2022-04-13 11:01:24 +0200

    link 2

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,4 +3,7 @@ class DashboardController < ApplicationController
     @reservations = Reservation.where(user_id:"#{current_user.id}")
     @plants = Plant.where(user_id:"#{current_user.id}")
   end
+
+  def destroy
+  end
 end
