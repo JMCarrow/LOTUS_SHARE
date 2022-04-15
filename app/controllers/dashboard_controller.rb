fd65091560e8d4fe5e3e883e984c2db6de7771df
Jin DU <telepathymira@gmail.com>
2022-04-15 10:45:56 +0200

    as an owner, we can now view client's reservations on the users'plants

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def dashboard
     @reservations = Reservation.where(user_id:"#{current_user.id}")
+    @clientreservations = Reservation.where(plant_id: current_user.plants.ids)
     @plants = Plant.where(user_id:"#{current_user.id}")
   end
 
