8cf3cb9aeb6459637b73d6ce631b1ce1073bd46e
RaoniPirat <raoni.pirat@gmail.com>
2022-04-15 16:04:27 +0200

    dashboard2 part1

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -17,13 +17,13 @@ class PlantsController < ApplicationController
     else
       @plants = Plant.all
     end
-    #@markers = @plants.geocoded.map do |plant|
-    #  {
-    #    lat: plant.latitude,
-    #    lng: plant.longitude,
-    #    info_window: render_to_string(partial: "info_window", locals: { plant: plant })
-    #  }
-    #end
+    @markers = @plants.geocoded.map do |plant|
+      {
+        lat: plant.latitude,
+        lng: plant.longitude,
+        #info_window: render_to_string(partial: "info_window", locals: { plant: plant })
+      }
+    end
   end
 
   def new
