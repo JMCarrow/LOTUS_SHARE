b3b98bd638c1c2d4198b705fef74fe4931bd6018
RaoniPirat <raoni.pirat@gmail.com>
2022-04-18 14:43:33 +0200

    user, map, index overview

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,48 +1,47 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
 
-    def index
-        @reviews = Review.all
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @plant = Plant.find(params[:plant_id])
+    @review.plant = @plant
+    authorize @review
+    if @review.save
+      redirect_to dashboard_path
+    else
+      render :new
     end
-    
-    def new
-        @review = Review.new
-    end
+  end
 
-    def create
-        @review = Review.new(review_params)
-        @review.user = current_user
-        @plant = Plant.find(params[:plant_id])
-        @review.plant = @plant
-        authorize @review
-        if @review.save
-          redirect_to dashboard_path
-        else
-          render :new
-        end
-      end
+  def edit
+    @review = Review.find(params[:id])
+    authorize @review
+  end
 
-      def edit
-        @review = Review.find(params[:id])
-        authorize @review
-      end
-    
-      def update
-        @review = Review.find(params[:id])
-        @review.update(review_params)
-        authorize @review
-        redirect_to dashboard_path
-      end
-    
-      def destroy
-        @review = Review.find(params[:id])
-        @review.destroy
-        authorize @review
-        redirect_to dashboard_path
-      end
+  def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    authorize @review
+    redirect_to dashboard_path
+  end
 
-      private
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    authorize @review
+    redirect_to dashboard_path
+  end
 
-      def review_params
-        params.require(:review, :rating).permit(:content, :plant_id, :user_id)
-      end
+  private
+
+  def review_params
+    params.require(:review, :rating).permit(:content, :plant_id, :user_id)
+  end
 end
