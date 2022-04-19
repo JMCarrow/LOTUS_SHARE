db3bbc2a5e6a7e65d747221aac617175154e2e49
RaoniPirat <raoni.pirat@gmail.com>
2022-04-19 17:21:56 +0200

    done but tired

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @plants = Plant.all
+    @plant = @plants[0..8]
+    @carousel1 = @plant[0..2]
+    @carousel2 = @plant[3..5]
+    @carousel3 = @plant[6..8]
   end
 end
